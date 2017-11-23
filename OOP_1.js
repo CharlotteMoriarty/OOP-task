130abaa6a92fc17fad3f4fb1fd6d534050eecd44
Karolina <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-23 17:26:08 +0100

    Update OOP_1.js

--- OOP_1.js
+++ OOP_1.js
@@ -3,10 +3,11 @@ function Phone(brand, color, memory, price) {
   this.color = color;
   this.price = price;
   this.memory = memory;
-    Phone.prototype.printInfo = function() {
-      console.log("The phone brand is " + this.brand + " the color is " + this.color + " memory in GB is " + this.memory + " and the price is " + this.price);
-    };
+     };  
 };
+Phone.prototype.printInfo = function() {
+ console.log("The phone brand is " + this.brand + " the color is " + this.color + " memory in GB is " + this.memory + " and the price is " + this.price);
+    
 var SamsungGalaxyS8 = new Phone ("Samsung",'black',64, 2400),
     iPhone6s = new Phone("Apple", 'gold',32, 1700),
     OnePlusOne = new Phone('OnePlus', 'black','-no data-',1300);  
